c4b30003f0c96301176878d527f203b624279a75
Edouard Paris <m@edouard.paris>
2018-07-20 15:31:18 +0200

    fix operation test Append

--- operation/operation_test.go
+++ operation/operation_test.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"encoding/hex"
 	"testing"
-)
 
-const hexDump = `f00688ac00000000`
+	"github.com/ulule/ancre/tag"
+)
 
 func TestNewOpAppend(t *testing.T) {
 	b, err := hex.DecodeString(`f00688ac00000000`)
@@ -15,6 +15,16 @@ func TestNewOpAppend(t *testing.T) {
 	}
 
 	r := bytes.NewReader(b)
+
+	ta, err := tag.GetByte(r)
+	if err != nil {
+		t.Errorf("Expected no error: \n %s", err.Error())
+	}
+
+	if ta != tag.Append {
+		t.Errorf("Expected equal tags")
+	}
+
 	op, err := NewOpAppend(r)
 	if err != nil {
 		t.Errorf("Expected no error: \n %s", err.Error())
